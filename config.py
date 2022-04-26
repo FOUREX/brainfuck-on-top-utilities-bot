91d5d7edb77879be6819c543bbb86824ffb369ec
FOUREX <76780880+FOUREX@users.noreply.github.com>
2022-04-26 23:58:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    "db": "mongodb+srv://bot:bot@cluster0.d5txo.mongodb.net/db?retryWrites=true&w=majority",
-    "token": "5392046701:AAHF016w6k1LYsKWW6Z_2dZDbNPpnE-Gz3I",
+    "db": "",
+    "token": "",
     "prefix": "!",
     "debug": False
 }
